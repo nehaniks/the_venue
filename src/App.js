77187d092ad2b1a0e0089f5649fcebeffdc4a3d8
Neha Nikhat <nehanikhat18@gmail.com>
2021-04-25 16:53:30 +0530

    Adding Carrousel

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./resources/styles.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Featured from "./components/featured";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <>
+        <Featured />
+      </>
       <div style={{ backgroundColor: "red", height: "800px" }}></div>
       <div style={{ backgroundColor: "blue", height: "800px" }}></div>
       <div style={{ backgroundColor: "green", height: "800px" }}></div>
