93e70b15c076a75da1d65781627fcb8b5821d248
Neha Nikhat <nehanikhat18@gmail.com>
2021-04-26 02:59:05 +0530

    Added Scroll to

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./resources/styles.css";
+import { Element } from "react-scroll";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -13,13 +14,21 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <>
+      <Element name="featured">
         <Featured />
-      </>
-      <VenueInfo />
-      <Highlights />
-      <Pricing />
-      <Location />
+      </Element>
+      <Element name="venueinfo">
+        <VenueInfo />
+      </Element>
+      <Element name="highlights">
+        <Highlights />
+      </Element>
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+      <Element name="location">
+        <Location />
+      </Element>
       <Footer />
     </div>
   );
